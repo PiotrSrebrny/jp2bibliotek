613dd2ed83185753ddb14c6c0ad10392ee7f93de
Piotr Srebrny <srebrny.piotr@gmail.com>
2017-10-15 21:16:31 +0200

    Add filtering of URI parameters

--- fuel/app/modules/util/classes/uri.php
+++ fuel/app/modules/util/classes/uri.php
@@ -4,10 +4,25 @@ namespace Util;
 
 class Uri
 {
+	static function filter_params()
+	{
+		$input = \Input::get();
+		$output = array();
+		foreach ($input as $key => $value) 
+		{
+			if (strlen($value) > 0) 
+			{
+				$output[$key] = $value;
+			}
+		}
+
+		return $output;
+	}
+
 	static function params()
 	{
-		$query = \Uri::build_query_string(\Input::get());
-	
+		$query = \Uri::build_query_string(\Util\Uri::filter_params());
+
 		if (strlen($query) > 0)
 			return  '?' . $query;
 		else
